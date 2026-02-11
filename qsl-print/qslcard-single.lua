96ec6d31fd1447a3da4d068572586d3053829664
Yū KOBAYASHI <kb10uy@kb10uy.org>
2026-02-11 20:50:20 +0900

    lto/strip

--- qsl-print/qslcard-single.lua
+++ qsl-print/qslcard-single.lua
@@ -1,11 +1,17 @@
----@param args table
+---@param args table<string,string>
 ---@param entries QslCardEntry[]
 local function generate(args, entries)
     local converted_entries = {}
     for i, e in ipairs(entries) do
+        local datetime = e.qso.datetime
+        if e.qso.mode ~= "FT8" then
+            datetime = datetime:to_offset("+09:00")
+        end
+
         table.insert(converted_entries, {
-            date = e.qso.datetime.date_str,
-            time = e.qso.datetime.time_str,
+            date = datetime.date_str,
+            time = datetime.time_str,
+            mode = e.qso.mode,
             call = e.qso.call,
         })
     end
